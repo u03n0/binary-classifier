26de7a53b00961c1d5fbef75d4a4378ac014a8f6
u03n0 <u03n0@proton.me>
2024-03-06 19:48:27 +0100

    restructured

--- src/models/bert_classifier.py
+++ src/models/bert_classifier.py
@@ -1,48 +1,72 @@
+import torch
 import pandas as pd
+
+from typing import Tuple, List
 from torch.optim import AdamW
 from torch.utils.data import DataLoader, random_split
-from models.features import build_dataset, load_model_tokenizer, get_features
+from models.features import build_dataset, get_features
 from pathlib import Path
-import torch
-
+from tqdm import tqdm
+from transformers import BertTokenizer, BertForSequenceClassification
 
 
 def run_model(df: pd.DataFrame):
 
     output_dir = Path("../models/fine_tuned_model/").resolve()
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    tokenizer, model = load_model_tokenizer(output_dir, device)
+    
+    model_exists = (output_dir / 'tokenizer_config.json').is_file()
+
+    train = True
+    if model_exists: # If model already saved, load it
+        train = False # no need to train
+        model = BertForSequenceClassification.from_pretrained(output_dir).to(device)
+        tokenizer = BertTokenizer.from_pretrained(output_dir)
+        print("Model and tokenizer loaded from: ", output_dir)
+
+    else:
+        print("Loading failed. Creating new model and tokenizer.")
+        model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2).to(device)
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+
+    train_dataloader, val_dataloader = build_dataloader(df, tokenizer, device)
     model.to(device)
+    torch.cuda.empty_cache()
 
-    dataset = build_dataset(tokenizer, get_features(df), device)
+    if train:
+        train_model(train_dataloader, model, tokenizer, output_dir)
 
-    train_size = int(0.8 * len(dataset))
-    val_size = len(dataset) - train_size
-    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+    return evaluate_model(val_dataloader, model)
 
-    train_dataloader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-    val_dataloader = DataLoader(val_dataset, batch_size=2, shuffle=False)
 
-    # Fine-tune the model
+def train_model(train_dataloader: DataLoader, model: BertForSequenceClassification, tokenizer: BertTokenizer, output_dir: Path):
+    """ Fine-tune the model 
+    """
     optimizer = AdamW(model.parameters(), lr=1e-5)
-    epochs = 3
+    epochs = 4
 
     for epoch in range(epochs):
         model.train()
+        train_dataloader = tqdm(train_dataloader, desc=f'Epoch {epoch + 1}/{epochs}', leave=False)
         for batch in train_dataloader:
             inputs = {'input_ids': batch[0], 'attention_mask': batch[1], 'labels': batch[2]}
             outputs = model(**inputs)
             loss = outputs.loss
             loss.backward()
             optimizer.step()
-            optimizer.zero_grad()
+            optimizer.zero_grad()    
+    
+    model.save_pretrained(output_dir, from_pt=True)
+    tokenizer.save_pretrained(output_dir, from_pt=True)
 
-    # Evaluate the model
+def evaluate_model(val_dataloader: DataLoader, model: BertForSequenceClassification)-> Tuple[List, List]:
+    """ evaluate the model 
+    """
     predictions = []
     true_labels = []
 
     with torch.no_grad():
-        for batch in val_dataloader:
+        for batch in tqdm(val_dataloader, desc='Validation', leave=False):
             inputs = {'input_ids': batch[0], 'attention_mask': batch[1], 'labels': batch[2]}
             outputs = model(**inputs)
             logits = outputs.logits
@@ -51,7 +75,18 @@ def run_model(df: pd.DataFrame):
             predictions.extend(predicted_labels.cpu().numpy())
             true_labels.extend(batch[2].detach().cpu().numpy())
 
-    
-    model.save_pretrained(output_dir, from_pt=True)
-
     return predictions, true_labels
+
+def build_dataloader(df: pd.DataFrame, tokenizer: BertTokenizer, device: torch.device)-> Tuple[DataLoader, DataLoader]:
+    """ Builds training and validation dataloaders
+    """
+    dataset = build_dataset(tokenizer, get_features(df), device)
+
+    train_size = int(0.8 * len(dataset))
+    val_size = len(dataset) - train_size
+    train_dataset, val_dataset = random_split(dataset, [train_size, val_size])
+
+    train_dataloader = DataLoader(train_dataset, batch_size=8, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, batch_size=8, shuffle=False)
+
+    return train_dataloader, val_dataloader
